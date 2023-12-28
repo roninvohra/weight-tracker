750e85a22f4fbd3881e94ad74f430911c77fe973
roninvohra <roninvohra19@gmail.com>
2023-12-28 01:51:28 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ReportWeight from './components/ReportWeight';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="flex text-3xl font-bold underline justify-center">
+        Weight Tracker
+      </h1>
+      <div className = "flex justify-center">
+        <ReportWeight />
+      </div>
+    </>
+  )
 }
 
 export default App;
