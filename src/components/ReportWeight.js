3f3acf124fa877146e1136e29c4931f12dc5fc2b
roninvohra <roninvohra19@gmail.com>
2023-12-29 19:47:05 -0800

    server

--- src/components/ReportWeight.js
+++ src/components/ReportWeight.js
@@ -2,20 +2,22 @@ import { useState } from 'react';
 
 function ReportWeight() {
   const [weight, setWeight] = useState('');
+  const [date, setDate] = useState('');
 
   let onButtonClick = () => {
-    alert (weight);
+    alert (weight, date);
+
   }
 
   return (
     <div className='bg-amber-200 box-border p-4 border-4 ...'>
       <span className='mr-10'>
-        Date: <input className='border rounded shadow-md' type = "date" id = "date" />
+        Date: <input className='border rounded shadow-md' onChange = {e => setDate(e.target.value)} type = "date" id = "date" />
       </span>
       <input onChange = {e => setWeight(e.target.value)} placeholder="Weight" className = 'mr-5 border rounded shadow-md w-20 h-10' type ="text" name = "weight" />  
-      <button type ="button" onClick = {onButtonClick} className = 'border rounded shadow-md bg-indigo-500 text-white w-20 h-10'>Submit</button>
+      <button type ="button" onClick = {onButtonClick} className = 'border rounded shadow-md bg-indigo-500 text-white w-20 h-10'>Add</button>
     </div>
   )
 }
 
-export default ReportWeight;
+export default ReportWeight; 
